3f65d4ef101c925fbe806081f73231722ad8c028
Kawa-1 <kamil-kawka@outlook.com>
2021-10-19 21:49:46 +0200

    Revert "Add some documentation"
    This reverts commit 3985495a1ca4bb282996accc5229644b8603dc96.

--- ex.py
+++ ex.py
@@ -1,27 +1,11 @@
 from __future__ import annotations
 
 def get_header(header: file_handler) -> dict:
-    """Read first line of file (header) to get the attributes of corresponding fields from data
-
-        Parameters
-        ----------
-        header: file_handler, _io.TextIOWrapper
-            We are passing just opened file
-
-        Returns
-        -------
-        dict
-            a dictionary of attributes as keys and corresponding indexes in data section as value
-            >>> {'CASE_NUMBER': 1, 'CASE_STATUS': 2, 'CASE_SUBMITTED': 3,...}"""
-    
     header = header.readline()
     header = header.split(';')
-    # removing unnecessary item from list; blank string
     header.pop(0)
     return {field.upper().rstrip('\n'): index
               for index, field in enumerate(header, start=1)}
-
-print(help(get_header))
 
 with open("input1.txt", "r") as f:
     header = get_header(f)
@@ -30,7 +14,8 @@ with open("input1.txt", "r") as f:
     for line in f:
         line = line.split(';')
         line.pop()
-        print(line[header.get("CASE_STATUS")])
+        print(line)
+        break
     
 
     
