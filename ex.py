6fd5c3705dbe0af442998d0440e4e9ed279e2bd7
Kawa-1 <kamil-kawka@outlook.com>
2021-10-20 12:14:29 +0200

    Adding idea with uszips.csv

--- ex.py
+++ ex.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+import time
 
 def get_header(header: file_handler) -> dict:
     """Read first line of file (header) to get the attributes of corresponding fields from data
@@ -25,6 +26,20 @@ print(help(get_header))
 def top_ten_occupations(filename: str):
     pass
 
+with open("others/uszips.csv", "r") as f:
+    start = time.perf_counter()
+    state_code = {}
+    # skip header
+    f.readline()
+    for nr_row, line in enumerate(f):
+        line = line.split(",")
+        # indexes: 0 - postal_code; 4 - state_id
+        state_code.update({line[0].strip("\""): line[4].strip("\"")})
+
+    end = time.perf_counter()
+    print(end - start)
+    print(state_code)
+
 
 with open("input_files/input1.txt", "r") as f:
     print(type(f))
@@ -34,7 +49,7 @@ with open("input_files/input1.txt", "r") as f:
     for line in f:
         line = line.split(';')
         line.pop()
-        print(line[header.get("WORKSITE_STATE")], line[header.get("WORKSITE_POSTAL_CODE")], "|", line[header.get("JOB_TITLE")],
+        print(line[header.get("WORKSITE_STATE")], line[header.get("WORKSITE_POSTAL_CODE")], "|", line[header.get("SOC_NAME")],
             "?", line[header.get("NAICS_CODE")], sep=" ")
     
 
